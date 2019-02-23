d3bf64acbed488607a583f52f52fc524e1cb5f31
Vladislav D <studio_kseven@mail.ru>
2019-02-23 20:44:36 +0300

    add update & delete posts

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,14 +1,8 @@
 const {validationResult} = require('express-validator/check')
-const { error } = require('../utils')
+const {error} = require('../utils')
 const Post = require('../models/post')
 
-exports.getPosts = (req, res, next) => {
-    Post
-        .find()
-        .then(posts => res.status(201).json(posts))
-        .catch(err => error({err, next}))
-}
-
+// create
 exports.createPost = (req, res, next) => {
     if (!validationResult(req).isEmpty()) {
 		error({
@@ -34,20 +28,60 @@ exports.createPost = (req, res, next) => {
 
     post
         .save()
-        .then(result => {
-            res.status(201).json({
-                message: 'Post create success',
-                post: result
-            })
-        })
+        .then(result => res.status(201).json(result))
         .catch(err => error({err, next}))
 }
 
-exports.getPost = (req, res, next) => {
-    const { id } = req.params
+// read
+exports.getAllPostsList = (req, res, next) => {
+    Post
+        .find()
+        .then(postsList => res.status(201).json(postsList))
+        .catch(err => error({err, next}))
+}
+
+exports.getPostById = (req, res, next) => {
+    const {id} = req.params
 
     Post
         .findById(id)
         .then(post => res.status(201).json(post))
         .catch(err => error({err, next}))
+}
+
+// update
+exports.updatePost = (req, res, next) => {
+	const {id} = req.params
+	const {
+		title,
+		content,
+		imageUrl,
+		creator
+	} = req.body
+
+	Post
+		.findById(id)
+		.then(post => {
+			post.title = title || post.title
+			post.content = content || post.content
+			post.imageUrl = imageUrl || post.imageUrl
+			post.creator = creator || post.creator
+
+			return post.save()
+		})
+		.then(result => res.status(201).json(result))
+		.catch(err => error({err, next}))
+}
+
+// delete
+exports.deletePost = (req, res, next) => {
+	const {id} = req.params
+
+	Post
+		.findById(id)
+		.then(post => {
+			return post.delete()
+		})
+		.then(result => res.status(201).json(result))
+		.catch(err => error({err, next}))
 }
--- routes/feed.js
+++ routes/feed.js
@@ -1,21 +1,29 @@
 const express = require('express')
-const { body } = require('express-validator/check')
+const {body} = require('express-validator/check')
 
 const feedController = require('../controllers/feed')
 
 const router = express.Router()
 
-router.get('/posts', feedController.getPosts)
-
+// create
 router.post('/post', [
     body('title')
         .trim()
         .isLength({min: 5}),
     body('content')
         .trim()
-        .isLength({min: 5}),
+		.isLength({min: 5})
 ], feedController.createPost)
 
-router.get('/post/:id', feedController.getPost)
+// read
+router.get('/posts', feedController.getAllPostsList)
+
+router.get('/post/:id', feedController.getPostById)
+
+// update
+router.put('/post/:id', feedController.updatePost)
+
+// delete
+router.delete('/post/:id', feedController.deletePost)
 
 module.exports = router
